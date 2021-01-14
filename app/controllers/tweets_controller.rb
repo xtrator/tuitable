c986d6241670081b4a4b942123c3b0c85fa37032
Alexander <alexfox_2000@hotmail.com>
2021-01-14 14:33:41 -0500

    Created Tweets#Show View

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,9 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all.where(replied_to: nil)
   end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    @tweets = @tweet.replies
+  end
 end
