e1f8f3a831ef017e431be7827390d32ec3b4d01a
susanluisa <susanluisacoari@gmail.com>
2021-01-14 16:04:00 -0500

    Added validations for tweet and user models

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,14 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  belongs_to :replied_to, optional: true, class_name: "Tweet", counter_cache: :replies_count
-  has_many :replies, class_name: "Tweet", foreign_key: :replied_to_id, dependent: :destroy
+  belongs_to :replied_to, optional: true, class_name: 'Tweet', counter_cache: :replies_count
+  has_many :replies, 
+           class_name: 'Tweet',
+           foreign_key: :replied_to_id,
+           dependent: :destroy,
+           inverse_of: :tweet
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
+
+  validates :body, presence: true, length: { maximum: 140 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :liked_tweets, through: :likes, source: :tweet
+
+  validates :username, :name, :password, :role, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true, length: { minimum: 6 }
+
+  enum role: { member: 0, admin: 1 }
 end
