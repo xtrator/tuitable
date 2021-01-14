d529840d41eccb3bf2f287800f3148cefdfcf18d
AndreValderrama <m.valderramae@pucp.edu.pe>
2021-01-14 16:59:40 -0500

    Add new fields to registration with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :avatar
+ 
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
 
