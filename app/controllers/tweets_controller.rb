5be8755a8b7bb8ef58bb140ffa68f7b99403bc4b
AndreValderrama <m.valderramae@pucp.edu.pe>
2021-01-15 07:27:48 -0500

    Add CRUD edit, destroy and update to tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,45 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
     @tweets = @tweet.replies
   end
+
+  def edit 
+    @tweet = Tweet.find(params[:id])
+  
+  end
+  
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    if @tweet.save
+      redirect_to @tweet
+    else
+      render :new
+    end
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    
+    if @tweet.update(tweet_params)
+      redirect_to @tweet
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    
+    @tweet.destroy
+
+    redirect_to @tweet
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:body)
+    end
+
+   
+  
 end
