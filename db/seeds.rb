5ceda65a8353bb6e032e6c17bb77ddefc48ba678
Alexander <alexfox_2000@hotmail.com>
2021-01-14 20:15:13 -0500

    created seeds for dummy user avatars

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ dogfire = User.create(email: 'dogfire@dogfire.com', password: 'thisisfine', name
 puts 'finished seeding users'
 
 puts 'started seeding tweets'
-thanos_tweet = thanos.tweets.create(body: Faker::Lorem.sentence( word_count: rand(15..20)) )
-goku_tweet = goku.tweets.create(body: Faker::Lorem.sentence( word_count: rand(15..20)) )
-dogfire_tweet = dogfire.tweets.create(body: Faker::Lorem.sentence( word_count: rand(15..20)) )
+thanos_tweet = thanos.tweets.create(body: Faker::Lorem.sentence( word_count: rand(15..16)) )
+goku_tweet = goku.tweets.create(body: Faker::Lorem.sentence( word_count: rand(15..16)) )
+dogfire_tweet = dogfire.tweets.create(body: Faker::Lorem.sentence( word_count: rand(15..16)) )
 puts 'finished seeding tweets'
 
 puts 'started seeding likes'
@@ -34,3 +34,9 @@ puts 'started seeding replies'
   Tweet.all.sample.replies.create( body: Faker::Lorem.sentence( word_count: rand(5..12) ), user: user)
 end
 puts 'finished seeding replies'
+
+puts 'started seeding avatars'
+thanos.avatar.attach(io: File.open('app/assets/images/thanos.png'), filename: 'thanos.png')
+goku.avatar.attach(io: File.open('app/assets/images/goku.png'), filename: 'goku.png')
+dogfire.avatar.attach(io: File.open('app/assets/images/dogfire.png'), filename: 'dogfire.png')
+puts 'finished seeding avatars'
