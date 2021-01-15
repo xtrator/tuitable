7b7a6193dbcf27a2bff5077d3e19f98c292ec0e4
Alexander <alexfox_2000@hotmail.com>
2021-01-14 20:18:43 -0500

    application rubocoped

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
- 
+
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
 
